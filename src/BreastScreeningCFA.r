12bf641013d60ee571f4ca732c5c3516b64db491
Francisco Maria Calisto <francisco_mcalisto@outlook.com>
2021-05-26 16:43:55 +0100

    [UPDATE] Source

--- src/BreastScreeningCFA.r
+++ src/BreastScreeningCFA.r
@@ -15,16 +15,21 @@ mydata <- SimplePre[c(10,11,12,13,
                       30,31,32,33,
                       34,35,36,37)]
 
-#Check the data
+# Check the data
 pairs.panels(mydata)
 corPlot(mydata)
 
-#Calculate the correlation matrix
+# Calculate the correlation matrix
 corMat <- cor(mydata)
 print(corMat)
 
-#Run the Factor Analsis
-fit_efa <- fa(mydata,8,fm="ml" ,rotate="oblimin")
+# RMSA < 0.08
+# Tucker Lewis Index < 0.99
+# If actor correlations are > 0.70 then we will
+# have divergence validity problem.
+
+# Run the Factor Analsis
+fit_efa <- fa(mydata, 7, fm="ml", rotate="oblimin")
 print(fit_efa, digits=2, cutoff=.3)
 
 model1 <- convert_efa_to_cfa(fit_efa, threshold = 0.3)
@@ -38,21 +43,69 @@ print(model1)
 # our factors and all of our items. The model is
 # not in our dataset, is something we create from
 # theory and literature.
-cfa_model_1 <- ' #start of model
+# The symbol =~ stands for construct.
+
+# TODO
+# Factors from literature = 10
+cfa_model_0 <- ' #start of model
 # latent variable definitions (common factors)
+  Impact =~
   PerfExp =~ Q10 + Q11 + Q12
   EffExp =~ Q13 + Q14 + Q15
   SocInf =~ Q16 + Q17 + Q18
   FacCond =~ Q19 + Q20 + Q21
-  IntUse =~ Q22 + Q23 + Q24
+  IntUse =~ Q22 + Q23 + Q24 + Q25
   Attitude =~ Q26 + Q27 + Q28
   Security =~ Q30 + Q31
   Privacy =~ Q32 + Q33 + Q34
   Trust =~ Q29 + Q35 + Q36 + Q37
 ' #end of model
 
-fit_cfa_1 <- cfa(cfa_model_1, data=mydata)
+fit_cfa_0 <- cfa(cfa_model_0, data=mydata, estimator="MLR", mimic="Mplus")
+summary(fit_cfa_0, fit.measures=TRUE, standardized=TRUE)
+
+# Factors from literature = 9
+cfa_model_1 <- ' #start of model
+# latent variable definitions (common factors)
+  PerfExp =~ Q10 + Q11 + Q12
+  EffExp =~ Q13 + Q14 + Q15
+  SocInf =~ Q16 + Q17 + Q18
+  FacCond =~ Q19 + Q20 + Q21
+  IntUse =~ Q22 + Q23 + Q24 + Q25
+  Attitude =~ Q26 + Q27 + Q28
+  Security =~ Q30 + Q31
+  Privacy =~ Q32 + Q33 + Q34
+  Trust =~ Q29 + Q35 + Q36 + Q37
+' #end of model
+
+fit_cfa_1 <- cfa(cfa_model_1, data=mydata, estimator="MLR", mimic="Mplus")
 summary(fit_cfa_1, fit.measures=TRUE, standardized=TRUE)
+
+# Factors from literature = 8
+cfa_model_2 <- ' #start of model
+# latent variable definitions (common factors)
+  PerfExp =~ Q10 + Q11 + Q12
+  EffExp =~ Q13 + Q14 + Q15
+  SocInf =~ Q16 + Q17 + Q18
+  FacCond =~ Q19 + Q20 + Q21
+  IntUse =~ Q22 + Q23 + Q24 + Q25 + Q26 + Q27 + Q28
+  Security =~ Q30 + Q31
+  Privacy =~ Q32 + Q33 + Q34
+  Trust =~ Q29 + Q35 + Q36 + Q37
+' #end of model
+
+# Latent variables = 8 (ordered by factor number)
+# ML1 =~ Q19 + Q20
+# ML2 =~ Q10 + Q22 + Q23 + Q24 + Q25
+# ML3 =~ Q35 + Q36 + Q37
+# ML4 =~ Q11 + Q12
+# ML5 =~ Q32 + Q33 + Q34
+# ML6 =~ Q26 + Q27 + Q28 + Q29 + Q30 + Q31
+# ML7 =~ Q13 + Q14 + Q15
+# ML8 =~ Q16 + Q17 + Q18 + Q19 + Q21
+
+fit_cfa_2 <- cfa(cfa_model_2, data=mydata, estimator="MLR", mimic="Mplus")
+summary(fit_cfa_2, fit.measures=TRUE, standardized=TRUE)
 
 ##################################################
 # TESTING END
