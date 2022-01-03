e2d191a5bbedaca16a42262108b9dfa353788409
Francisco Maria Calisto <francisco_mcalisto@outlook.com>
2022-01-03 15:09:38 +0000

    Update Radiomics_changed.r

--- src/Radiomics_changed.r
+++ src/Radiomics_changed.r
@@ -64,7 +64,8 @@ loadings.m <- melt(loadings, id="",
                    measure=c("Factor 1", "Factor 2", "Factor 3",
                              "Factor 4", "Factor 5", "Factor 6",
                              "Factor 7", "Factor 8"),
-                   variable.name="Factor", value.name="Loading")
+                   variable.name="Factor",
+                   value.name="Loading")
 
 colnames(loadings.m)[1] <- "Test"
 colnames(loadings.m)[2] <- "Factor"
@@ -172,116 +173,3 @@ miPowerFit(fit_cfa, stdLoad = 0.4, cor = 0.1, stdBeta = 0.1,
            intcept = 0.2, stdDelta = NULL, delta = NULL, cilevel = 0.9)
 
 calEffSizes(model=cfa_model, n=mydata.n, Cov=mydata.cor, lavaan.output=FALSE)
-
-
-mydata2 <- MadeiraSafeAgo[c(3,4,5,52,54,26,27,28,30,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51)]
-
-#Group by Age
-fit_cfa_age_1 <- cfa(cfa_model, data=mydata2, estimator = "WLSMV", group="Age2")
-fit_cfa_age_2 <- cfa(cfa_model, data=mydata2, group="Age2", estimator = "WLSMV", group.equal = "loadings")
-fit_cfa_age_3 <- cfa(cfa_model, data=mydata2, group="Age2", estimator = "WLSMV", group.equal = c("intercepts", "loadings"))
-summary(fit_cfa_age_1, fit.measures=TRUE, standardized=TRUE)
-
-compareFit(fit_cfa_age_1, fit_cfa_age_2)
-compareFit(fit_cfa_age_2, fit_cfa_age_3)
-
-lavTestLRT(fit_cfa_age_1, fit_cfa_age_2, fit_cfa_age_3)
-
-anova(fit_cfa_age_1, fit_cfa_age_2, fit_cfa_age_3)
-
-lavTestScore(fit_cfa_age_2)
-
-fitMeasures(fit_cfa_age_1, c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "srmr"),
-            output = "text")
-fitMeasures(fit_cfa_age_2, c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "srmr"),
-            output = "text")
-fitMeasures(fit_cfa_age_3, c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "srmr"),
-            output = "text")
-
-#Group by Gender
-fit_cfa_gender_1 <- cfa(cfa_model, data=mydata2, group="Gender")
-fit_cfa_gender_2 <- cfa(cfa_model, data=mydata2, group="Gender", group.equal = "loadings")
-fit_cfa_gender_3 <- cfa(cfa_model, data=mydata2, group="Gender", group.equal = c("intercepts", "loadings"))
-lavTestLRT(fit_cfa_gender_1, fit_cfa_gender_2, fit_cfa_gender_3)
-summary(fit_cfa_gender_1, fit.measures=TRUE, standardized=TRUE)
-measurementInvariance(model=cfa_model,data=mydata2, group="Gender")
-fitMeasures(fit_cfa_gender_1, c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "srmr"),
-            output = "text")
-fitMeasures(fit_cfa_gender_2, c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "srmr"),
-            output = "text")
-fitMeasures(fit_cfa_gender_3, c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "srmr"),
-            output = "text")
-
-#Group by Nationality
-fit_cfa_nac_1 <- cfa(cfa_model, data=mydata2, estimator = "WLSMV", group="Nat2")
-fit_cfa_nac_2 <- cfa(cfa_model, data=mydata2, estimator = "WLSMV", group="Nat2", group.equal = "loadings")
-fit_cfa_nac_3 <- cfa(cfa_model, data=mydata2, estimator = "WLSMV", group="Nat2", group.equal = c("intercepts", "loadings"))
-lavTestLRT(fit_cfa_nac_1, fit_cfa_nac_2, fit_cfa_nac_3)
-summary(fit_cfa, fit.measures=TRUE, standardized=TRUE)
-fitMeasures(fit_cfa, c("chisq","df","cfi","rmsea","srmr","agfi","gfi","tli"), output = "matrix")
-measurementInvariance(model=cfa_model,data=mydata2, group="Nat2")
-
-fitMeasures(fit_cfa_nac_1, c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "srmr"),
-            output = "text")
-fitMeasures(fit_cfa_nac_2, c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "srmr"),
-            output = "text")
-fitMeasures(fit_cfa_nac_3, c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "srmr"),
-            output = "text")
-
-#Group by Regularity
-fit_cfa_reg_1 <- cfa(cfa_model, data=mydata2, estimator = "WLSMV", group="Local")
-fit_cfa_reg_2 <- cfa(cfa_model, data=mydata2, estimator = "WLSMV", group="Local", group.equal = "loadings")
-fit_cfa_reg_3 <- cfa(cfa_model, data=mydata2, estimator = "WLSMV", group="Local", group.equal = c("intercepts", "loadings"))
-lavTestLRT(fit_cfa_reg_1, fit_cfa_reg_2, fit_cfa_reg_3)
-
-summary(fit_cfa_reg_1, fit.measures=TRUE, standardized=TRUE)
-measurementInvariance(model=cfa_model,data=mydata2, group="Local")
-
-fitMeasures(fit_cfa_reg_1, c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "srmr"),
-            output = "text")
-fitMeasures(fit_cfa_reg_2, c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "srmr"),
-            output = "text")
-fitMeasures(fit_cfa_reg_3, c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "srmr"),
-            output = "text")
-
-#Group by Education
-fit_cfa_edu_1 <- cfa(cfa_model, data=mydata2, estimator = "WLSMV", group="Edu2")
-fit_cfa_edu_2 <- cfa(cfa_model, data=mydata2, estimator = "WLSMV", group="Edu2", group.equal = "loadings")
-fit_cfa_edu_3 <- cfa(cfa_model, data=mydata2, estimator = "WLSMV", group="Edu2", group.equal = c("intercepts", "loadings"))
-lavTestLRT(fit_cfa_edu_1, fit_cfa_edu_2, fit_cfa_edu_3)
-
-summary(fit_cfa, fit.measures=TRUE, standardized=TRUE)
-measurementInvariance(model=cfa_model,data=mydata2, group="Edu2")
-
-fitMeasures(fit_cfa_edu_1, c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "srmr"),
-            output = "text")
-fitMeasures(fit_cfa_edu_2, c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "srmr"),
-            output = "text")
-fitMeasures(fit_cfa_edu_3, c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "srmr"),
-            output = "text")
-
-cfa_model <- ' #start of model
-# latent variable definitions (common factors)
-  ImpactCV =~ Q2+Q3+Q4
-  SafetyCV =~ Q6+Q8+Q9
-  PerfExp =~ Q11+Q12
-  EffExp =~ Q13+Q14
-  SocInf =~ Q15+Q16
-  FacCond =~ Q17+Q18
-  IntUse =~ Q19+Q20
-  Security =~ Q21+Q22+Q23
-  Privacy =~ Q24+Q25
-  Trust =~ Q26+Q27
-# regressions
-  SafetyCV ~ ImpactCV
-  Security ~ Privacy + SocInf + SafetyCV
-  Trust ~ ImpactCV + Privacy + Security
-  EffExp ~ SocInf + FacCond + Trust
-  PerfExp ~ SocInf + EffExp + Privacy + Trust
-  IntUse ~ PerfExp + FacCond + Security + SafetyCV
-' #end of model
-
-fit_cfa_3 <- cfa(cfa_model_3, data=mydata3)
-summary(fit_cfa_3, fit.measures=TRUE, standardized=TRUE)
-reliability(fit_cfa_3)
-fitted(fit_cfa_3)
